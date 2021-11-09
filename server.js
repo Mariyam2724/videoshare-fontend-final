df6c6e6ae962291459cbd3008c05d0047a0d6d80
m-triple-m <triplem656@gmail.com>
2021-11-09 07:18:16 +0530

    ..

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
 }
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(requireHTTPS);
+const path = require("path");
 
-app.use(express.static('./dist/frontend'));
+app.use(express.static(path.join(__dirname, "/dist/frontend")));
 
-app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/frontend/' }
-    );
+app.get("/*", function (req, res) {
+  res.sendFile("index.html", { root: "dist/frontend/" });
 });
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log('server started on ' + port));
+app.listen(port, () => console.log("server started on " + port));
