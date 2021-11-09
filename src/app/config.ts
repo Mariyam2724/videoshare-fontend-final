46513fd083679f10bfc80cc0296b0eda5913a80b
m-triple-m <triplem656@gmail.com>
2021-11-09 07:00:42 +0530

    ..

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export const app_config ={ 
 
     // api_url : 'http://localhost:3000',
-    // api_url : 'https://videoshare27-api.herokuapp.com',
-    api_url : 'https://video-hosting-api.herokuapp.com'
+    api_url : 'https://videoshare27-api.herokuapp.com',
+    // api_url : 'https://video-hosting-api.herokuapp.com'
 };
